c0802b103b009afcfc98be6d31e6a1aff901ad95
iaas22 <ishaaamin2@gmail.com>
2026-05-22 19:10:47 +0200

    validación final de todos los endpoints

--- projecte3/src/main/java/com/ra5/projecte3/Projecte3Application.java
+++ projecte3/src/main/java/com/ra5/projecte3/Projecte3Application.java
@@ -1,9 +1,11 @@
 package com.ra5.projecte3;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Projecte3Application {
-
-	public static void main(String[] args) {
-		System.out.println("Projecte3Application started (Spring Boot not present)");
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Projecte3Application.class, args);
+    }
 }
